c1807f71655e160d3b9f3f2096a81eadf9e7f7ac
Nakxh <hakanjackcimen@gmail.com>
2021-07-17 16:16:23 +0100

    added char list

--- app.js
+++ app.js
@@ -9,32 +9,36 @@ const responce = document.querySelector(".responce");
 const profile = document.querySelector(".profile");
 const imageEl = document.querySelector(".image");
 const didYouMeanEl = document.querySelector(".didYouMean");
+const charSelect = document.querySelector(".charSelect")
+const charApiUrl = "https://rickandmortyapi.com/api/character";
+const charList = document.querySelector(".charList");
 
-
-submit.addEventListener("click",(e) => {
+if(submit) {
+  submit.addEventListener("click",(e) => {
   
-  console.log(characterId.value);
- 
-  e.preventDefault()
-
-  fetch(`${apiUrl}${characterId.value}`)
-    .then((responce) => {
-      return responce.json();
-    })
-    .then(data => {
-      console.log(data);
-      buildProfile(data.results[0])
-      buildDidYouMean(data.results)
-    })
-
-});
+    console.log(characterId.value);
+   
+    e.preventDefault()
+  
+    fetch(`${apiUrl}${characterId.value}`)
+      .then((responce) => {
+        return responce.json();
+      })
+      .then(data => {
+        console.log(data);
+        buildProfile(data.results[0])
+        buildDidYouMean(data.results)
+      })
+  
+  });
+}
 
 function buildSection(element, value){
-  element.innerHTML=value
+  element.innerHTML = value
 };
 
 function buildImageSection(element, value){
-  element.src=value
+  element.src = value
 };
 
 function buildProfile(result){
@@ -56,4 +60,31 @@ function buildDidYouMean(results){
     })
     didYouMeanEl.appendChild(item);
   }
+};
+
+function loadCharacters() {
+  fetch(`${charApiUrl}`)
+    .then((responce) => {
+      return responce.json();
+    })
+    .then(data => {
+      console.log(data);
+      buildCharList(charList, data.results)
+    })
+};
+
+if(charList) {
+  loadCharacters();
+};
+
+function buildCharList(element, value){
+  element.innerHTML = ""
+  value.forEach(char => {
+    const item = document.createElement("li");
+    item.innerHTML = `${char.name}`
+    element.appendChild(item);
+    item.addEventListener("click", (e) => {
+      buildProfile(char)
+    })
+  });
 };
